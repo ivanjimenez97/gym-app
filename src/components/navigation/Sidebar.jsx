4f658c711d1e828c07fe4b07c3b53d5c5dee4574
ivanjimenez97 <ivaneduardojimenezleon@gmail.com>
2024-10-01 21:51:46 -0600

    New Icon was added to the sidebar.

--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { Link, NavLink } from "react-router-dom";
 import MdiLightViewDashboard from "../icons/MdiLightViewDashboard";
 import UsersGroupSolid from "../icons/UsersGroupSolid";
+import BarbellIcon from "../icons/BarbellIcon.jsx";
 import { SubscriptionIcon } from "../icons/SubscriptionIcon";
 import { MembersIcon } from "../icons/MembersIcon";
 import { PlansIcon } from "../icons/PlansIcon";
@@ -8,18 +9,20 @@ import { PlansIcon } from "../icons/PlansIcon";
 export default function Sidebar() {
   return (
     <aside className="sidebar bg-white lg:w-[240px] min-h-screen hidden md:block">
-      <Link to={"/"} className="flex items-center py-3 px-5">
-        <h2 className="text-xl text-center pt-3 px-3 mb-5 text-indigo-600 font-bold leading-5 hidden lg:block">
-          Gym App
-        </h2>
+      <Link
+        to={"/"}
+        className="flex justify-center items-center pt-5 pb-3 px-5 text-2xl text-center mb-2 text-indigo-700 font-bold leading-5"
+      >
+        <BarbellIcon className="w-[1.75rem] h-[1.75rem]" />{" "}
+        <span className="hidden lg:block px-3">Gym App</span>
       </Link>
       <NavLink
         to={"/dashboard"}
         activeClassName="border-r-5 border-indigo-500 font-bold text-indigo-500"
         className={({ isActive }) =>
           isActive
-            ? "border-r-4 border-indigo-500 font-bold text-indigo-500 flex items-center py-3 px-5"
-            : "text-gray-500 hover:text-indigo-400 flex items-center py-3 px-5"
+            ? "border-r-4 border-indigo-500 font-bold text-indigo-500 flex justify-center lg:justify-start items-center py-3 px-5"
+            : "text-gray-500 hover:text-indigo-400 flex justify-center lg:justify-start items-center py-3 px-5"
         }
       >
         <MdiLightViewDashboard className="w-[1.75rem] h-[1.75rem]" />
@@ -30,11 +33,11 @@ export default function Sidebar() {
         to={"/plans"}
         className={({ isActive }) =>
           isActive
-            ? "border-r-4 border-indigo-500 font-bold text-indigo-500 flex items-center py-3 px-5"
-            : "text-gray-500 hover:text-indigo-400 flex items-center py-3 px-5"
+            ? "border-r-4 border-indigo-500 font-bold text-indigo-500 flex justify-center lg:justify-start items-center py-3 px-5"
+            : "text-gray-500 hover:text-indigo-400 flex justify-center lg:justify-start items-center py-3 px-5"
         }
       >
-        <PlansIcon />
+        <PlansIcon className="w-[1.50rem] h-[1.50rem]" />
         <span className="hidden lg:block px-3">Planes</span>
       </NavLink>
 
@@ -42,11 +45,11 @@ export default function Sidebar() {
         to={"/subscriptions"}
         className={({ isActive }) =>
           isActive
-            ? "border-r-4 border-indigo-500 font-bold text-indigo-500 flex items-center py-3 px-5"
-            : "text-gray-500 hover:text-indigo-400 flex items-center py-3 px-5"
+            ? "border-r-4 border-indigo-500 font-bold text-indigo-500 flex justify-center lg:justify-start items-center py-3 px-5"
+            : "text-gray-500 hover:text-indigo-400 flex justify-center lg:justify-start items-center py-3 px-5"
         }
       >
-        <SubscriptionIcon />
+        <SubscriptionIcon className="w-[1.50rem] h-[1.50rem]" />
         <span className="hidden lg:block px-3">Suscripciones</span>
       </NavLink>
 
@@ -54,11 +57,11 @@ export default function Sidebar() {
         to={"/members"}
         className={({ isActive }) =>
           isActive
-            ? "border-r-4 border-indigo-500 font-bold text-indigo-500 flex items-center py-3 px-5"
-            : "text-gray-500 hover:text-indigo-400 flex items-center py-3 px-5"
+            ? "border-r-4 border-indigo-500 font-bold text-indigo-500 flex justify-center lg:justify-start items-center py-3 px-5"
+            : "text-gray-500 hover:text-indigo-400 flex justify-center lg:justify-start items-center py-3 px-5"
         }
       >
-        <MembersIcon />
+        <MembersIcon className="w-[1.50rem] h-[1.50rem]" />
         <span className="hidden lg:block px-3">Miembros</span>
       </NavLink>
 
@@ -66,8 +69,8 @@ export default function Sidebar() {
         to={"/users"}
         className={({ isActive }) =>
           isActive
-            ? "border-r-4 border-indigo-500 font-bold text-indigo-500 flex items-center py-3 px-5"
-            : "text-gray-500 hover:text-indigo-400 flex items-center py-3 px-5"
+            ? "border-r-4 border-indigo-500 font-bold text-indigo-500 flex justify-center lg:justify-start items-center py-3 px-5"
+            : "text-gray-500 hover:text-indigo-400 flex justify-center lg:justify-start items-center py-3 px-5"
         }
       >
         <UsersGroupSolid className="w-[1.75rem] h-[1.75rem]" />
